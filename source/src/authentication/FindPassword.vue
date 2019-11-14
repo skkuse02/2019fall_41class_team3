95e214cf042007ced2116a4c18fd632193878fc7
seongkyulim <96x60812@naver.com>
2019-11-14 21:08:15 +0900

    [Add] input style, button style
    add input and find button

---
+++ source/src/authentication/FindPassword.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <div class="input">
+      Id <input type="text" name="Id"><br>
+      Name <input type="text" name="Name"><br>
+      email <input type="text" name="email"><br>
+    </div>
+    <div class="searchdb">
+      <button type="button" name="Find">Find</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: "Main",
+  components: {
+
+  }
+};
+</script>
