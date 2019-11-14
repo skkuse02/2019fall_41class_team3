7e460f9a8bdf3df0dae676f6efc76bd962fe47e5
devAdaid <ahyeong2533@gmail.com>
2019-11-14 21:22:27 +0900

    [Edit] Header (working)

--- source/src/containers/Header.vue
+++ source/src/containers/Header.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
-      <b-navbar-brand href="#"><img id="header-logo" src="assets/img/logo_white_typeC.png"></b-navbar-brand>
+    <b-navbar id="header-bar">
+      <b-navbar-brand id="header-brand" href="#"><img id="header-logo" src="assets/img/logo_white_typeC.png"></b-navbar-brand>
+      <div id="header-right-bar">
+          <router-link :to="{ path: '/'}">Log In</router-link>
+          <span> | </span>
+          <router-link :to="{ path: '/'}">Sign Up</router-link>
+      </div>
     </b-navbar>
   </div>
 </template>
