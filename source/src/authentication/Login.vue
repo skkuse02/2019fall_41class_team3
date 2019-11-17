dbc5c9ed9222ecc69365594094d621fed955348a
D-Sinus <peoplebj@naver.com>
2019-11-17 20:52:23 +0900

    [Add] POST method + Apply local style

--- source/src/authentication/Login.vue
+++ source/src/authentication/Login.vue
@@ -1,34 +1,75 @@
 <template>
-  <div>
-    <form method="post" id="loginForm" name="loginForm" action=>
+  <div class="loginForm">
+    <h2>Login</h2><br>
+    <form @submit.prevent="login(uid, password)">
         <div>
-            <label for="uid">아이디</label>
-            <input type="text" id="uid" name="uid" placeholder="ID" pattern="^([a-zA-Z0-9_]){8,20}$" >
+            <b-form-group label="아이디">
+              <b-form-input type="text" v-model="uid" placeholder="ID"
+              pattern="^([a-zA-Z0-9_]){8,20}$"></b-form-input>
+            </b-form-group>
         </div>
         <div>
-            <label for="password">비밀번호</label>
-            <input type="password" id="password" name="password" placeholder="Password" >
+            <b-form-group label="비밀번호">
+              <b-form-input type="password" v-model="password" placeholder="Password"
+              pattern="^([a-zA-Z0-9_]){8,20}$"></b-form-input>
+            </b-form-group>
         </div>
-        <button type="submit">로그인</button>
-        <button>ID 찾기</button>
-        <button>PW 찾기</button>
-        <div>
-            <input type="checkbox" id="loginSustain" name="loginSustain">
-            <label for="loginSustain"><span>로그인 상태 유지</span></label>
-        </div>
-    </form>
+        <b-button type="submit" variant="success" size="lg">로그인</b-button>
+        <router-link to='../findid'>
+          <b-button variant="success" size="lg">ID 찾기</b-button>
+        </router-link>
+        <router-link to='../findpassword'>
+          <b-button variant="success" size="lg">PW 찾기</b-button>
+        </router-link>
+      </form>
+      <div>
+          <input type="checkbox" id="loginSustain" name="loginSustain">
+          <label for="loginSustain"><span>로그인 상태 유지</span></label>
+      </div>
   </div>
 </template>
 
 <script>
 
-
 export default {
   name: "Login",
-  components: {
+  data () {
+    return {
+      uid: '',
+      password: '',
+    }
+  },
+  methods: {
+    login () {
+      const uid = this.uid;
+      const password = this.password;
 
-  }
+      if (!uid || !password) {
+        alert('아이디와 비밀번호를 입력해주세요!')
+      }
+
+      axios.post("http://localhost:3000/rest/login", {uid, password})
+        .then(res => {
+          if (res.status == 200){
+            alert('환영합니다!' + this.uid + '!')
+          }
+        })
+        .catch(err => {
+          alert('아이디나 비밀번호를 다시 한 번 확인해주세요!')
+        })
+    }
+  },
 };
 </script>
 
-        
+<style scoped>
+    h2 {
+        font-weight: normal;
+    }
+    a {
+        color: #42b983;
+    }
+    .btn-lg {
+        width: 33.3%;
+    }
+</style>
