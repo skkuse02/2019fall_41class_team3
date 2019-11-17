6a8336763b5eb6e7163d68d8bbf64b08729b09a7
D-Sinus <peoplebj@naver.com>
2019-11-17 21:12:53 +0900

    [Add] Some comments for Login.vue

--- source/src/authentication/Login.vue
+++ source/src/authentication/Login.vue
@@ -4,24 +4,34 @@
     <form @submit.prevent="login(uid, password)">
         <div>
             <b-form-group label="아이디">
+              <!-- ID should consists of uppercase + lowercase alphabet + number -->
+              <!-- It's length must be 8 to 20 -->
               <b-form-input type="text" v-model="uid" placeholder="ID"
               pattern="^([a-zA-Z0-9_]){8,20}$"></b-form-input>
             </b-form-group>
         </div>
         <div>
             <b-form-group label="비밀번호">
+              <!-- For password, same rule with ID -->
               <b-form-input type="password" v-model="password" placeholder="Password"
               pattern="^([a-zA-Z0-9_]){8,20}$"></b-form-input>
             </b-form-group>
         </div>
+
+        <!-- Pressing login button will send form's info to server -->
         <b-button type="submit" variant="success" size="lg">로그인</b-button>
+
+        <!-- Pressing findid button will redirect user to findid page -->
         <router-link to='../findid'>
           <b-button variant="success" size="lg">ID 찾기</b-button>
         </router-link>
+
+        <!-- Pressing findpw button will redirect user to findpw page -->
         <router-link to='../findpassword'>
           <b-button variant="success" size="lg">PW 찾기</b-button>
         </router-link>
       </form>
+      <!-- Sustain login feature is not implemented yet -->
       <div>
           <input type="checkbox" id="loginSustain" name="loginSustain">
           <label for="loginSustain"><span>로그인 상태 유지</span></label>
@@ -62,6 +72,7 @@ export default {
 };
 </script>
 
+<!-- Local style for login page -->
 <style scoped>
     h2 {
         font-weight: normal;
