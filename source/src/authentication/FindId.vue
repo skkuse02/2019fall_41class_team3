f364e5f08906d812772ca26aac42ff80a05e30ca
seongkyulim <96x60812@naver.com>
2019-11-27 22:52:40 +0900

    [Fix] Function error

--- source/src/authentication/FindId.vue
+++ source/src/authentication/FindId.vue
@@ -27,7 +27,7 @@ export default {
         email:''
       }
     }
-  }
+  },
 
   methods: {
     //methods error
--- source/src/authentication/FindPassword.vue
+++ source/src/authentication/FindPassword.vue
@@ -31,7 +31,7 @@ export default {
         email:''
       }
     }
-  }
+  },
   methods: {
     //methods error
     submit () {
