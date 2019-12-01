ebdd5f4b1a342cef020ffee40893e8aba28975ec
devAdaid <ahyeong2533@gmail.com>
2019-12-01 20:07:20 +0900

    [Edit] Register page style and validation

--- source/src/authentication/Register.vue
+++ source/src/authentication/Register.vue
@@ -1,53 +1,63 @@
 <template>
   <div>
     <h1>Sign Up</h1>
-    <form class="register-form" @submit.prevent="register()">
-        <div v-show="currentStep==0">
-            <selection>
-              <div>* Type</div>
-              <input type="radio" v-model="type" value="Mentor">Mentor
-              <input type="radio" v-model="type" value="Mentee">Mentee
-            </selection>
+    <form class="layout-form" @submit.prevent="register()">
+
+        <!-- Step 1: Select user type -->
+        <div class="form-group" v-show="currentStep==0">
+          <div class="label">* Type</div>
+          <div class="content">
+            <label>
+              <input type="radio" v-model="type" value="Mentor"/>Mentor
+            </label>
+            <label>
+              <input type="radio" v-model="type" value="Mentee"/>Mentee
+            </label>
+          </div>
         </div>
 
+        <!-- Step 2: Input ID & Password -->
         <div v-show="currentStep==1">
           <div class="form-group" :class="{error: validation.hasError('uid')}">
-            <div>* ID</div>
+            <div class="label">* ID</div>
             <div class="content"><input type="id" class="form-control" v-model="uid"/></div>
             <div class="message">{{ validation.firstError('uid') }}</div>
           </div>
           <div class="form-group" :class="{error: validation.hasError('password')}">
-            <div>* Password</div>
+            <div class="label">* Password</div>
             <div class="content"><input type="password" class="form-control" v-model="password"/></div>
             <div class="message">{{ validation.firstError('password') }}</div>
           </div>
           <div class="form-group" :class="{error: validation.hasError('repeat')}">
-            <div>* Confirm password</div>
+            <div class="label">* Confirm password</div>
             <div class="content"><input type="password" class="form-control" v-model="repeat"/></div>
             <div class="message">{{ validation.firstError('repeat') }}</div>
           </div>
         </div>
 
+        <!-- Step 3:  -->
         <div v-show="currentStep==2">
             <div class="form-group" :class="{error: validation.hasError('name')}">
-              <div>* Name</div>
+              <div class="label">* Name</div>
               <div class="content"><input type="text" class="form-control" v-model="name"/></div>
               <div class="message">{{ validation.firstError('name') }}</div>
             </div>
             <div class="form-group" :class="{error: validation.hasError('nicknname')}">
-              <div>* Nickname</div>
+              <div class="label">Nickname</div>
               <div class="content"><input type="text" class="form-control" v-model="nickname"/></div>
               <div class="message">{{ validation.firstError('nickname') }}</div>
             </div>
             <div class="form-group" :class="{error: validation.hasError('email')}">
-              <div>* Email</div>
+              <div class="label">* Email</div>
               <div class="content"><input type="text" class="form-control" v-model="email"/></div>
               <div class="message">{{ validation.firstError('email') }}</div>
             </div>
         </div>
 
-        <div v-show="currentStep==3">
-            <p><input type="text" v-model="field" placeholder="Field"></p>
+        <!--  -->
+        <div class="form-group" v-show="currentStep==3">
+            <div class="label">Field</div>
+            <div class="content"><input type="text" v-model="field" placeholder="Field"></div>
         </div>
 
         <div>
@@ -101,10 +111,10 @@ export default {
   },
   validators: {
     uid: function (value) {
-      return  this.$validator.value(value).required().minLength(5);
+      return  this.$validator.value(value).required().minLength(5).regex('^[A-Za-z0-9]*$', 'Must only contain alphabet letters or numbers.');
     },
     password: function (value) {
-      return  this.$validator.value(value).required().minLength(8);
+      return  this.$validator.value(value).required().minLength(8).regex('^[A-Za-z0-9]*$', 'Must only contain alphabet letters or numbers.');
     },
     'repeat, password': function (repeat, password) {
       if (this.submitted || this.validation.isTouched('repeat')) {
@@ -115,7 +125,7 @@ export default {
       return this.$validator.value(value).required().regex('^[A-Za-z]*$', 'Must only contain alphabetic characters.');
     },
     nickname: function(value) {
-      return this.$validator.value(value).required().regex('^[A-Za-z]*$', 'Must only contain alphabetic characters.');
+      return this.$validator.value(value);
     },
     email: function (value) {
       return this.$validator.value(value).required().email();
