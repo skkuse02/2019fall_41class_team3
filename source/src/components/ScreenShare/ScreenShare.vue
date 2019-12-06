d55950e0483f72396cbc615614a0d71f2d8b51fc
EunChan Park <pec9399@naver.com>
2019-12-07 02:40:36 +0900

    [Edit] Screen share

--- source/src/components/ScreenShare/ScreenShare.vue
+++ source/src/components/ScreenShare/ScreenShare.vue
@@ -1,12 +1,6 @@
 <template>
   <div class="container">
     <div class="row">
-      <div class="col-md-12 my-3">
-        <h2>Room</h2>
-        <input v-model="roomId">
-      </div>
-    </div>
-    <div class="row">
       <div class="col-md-12">
         <div class="">
           <vue-webrtc ref="webrtc"
@@ -57,15 +51,35 @@
     data() {
       return {
         img: null,
-        roomId: "public-room",
+        roomId: '',
         socketURL: 'https://rtcmulticonnection.herokuapp.com:443/',
         enableAudio: true,
-        enableVideo: true
+        enableVideo: false,
+        question: {
+                title: '',
+                content: ''
+        },
+        time : null
       };
     },
-    computed: {
-    },
-    watch: {
+    mounted: async function(){
+      try{
+        const user = await this.$http.get('/rest/user/session');
+        this.user = user.data.user.uid;
+      } catch(e){
+        this.$router.push('/login');
+      }
+      try{
+        const question = await this.$http.get('/rest/question/arranged/' + this.$route.query.qid, this.$route.query.room);
+        this.question.title = question.data.question.title;
+        this.question.content = question.data.question.content;
+        this.time = question.data.arranged;
+        this.roomId = this.$route.query.room;
+        this.$refs.webrtc.join();
+        this.img = this.$refs.webrtc.shareScreen();
+      } catch(e){
+            this.$router.go(-1);
+      }
     },
     methods: {
       onCapture() {
