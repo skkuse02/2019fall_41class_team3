cb9d3efad26c61412ddb5c678e650ee84727a680
devAdaid <ahyeong2533@gmail.com>
2019-12-06 13:15:41 +0900

    [Edit] Search title&uid, recommend question

--- source/src/views/question/QuestionList.vue
+++ source/src/views/question/QuestionList.vue
@@ -1,31 +1,65 @@
 <template>
   <div class="container">
-    <div class="card">
+    <div class="card" v-if="isMentor&&(recomendedTable.totalRows>0)">
       <div class="card-body">
-        <div style="width:200px;">
-          <b-input-group size="sm">
-            <b-form-input
-              v-model="filter"
-              type="search"
-              id="filterInput"
-              placeholder="Type to Search"
-            ></b-form-input>
-            <b-input-group-append>
-              <b-button :disabled="!filter" @click="filter = ''">Clear</b-button>
-            </b-input-group-append>
-          </b-input-group>
-        </div>
         <b-row>
           <b-col lg="12">
             <b-table class="mb-0 table-outline" style="text-align: center" responsive="lg" hover 
-            :items="questions"
-            :fields="fields"
-            :sort-by.sync="sortBy"
-            :sort-desc.sync="sortDesc"
-            :current-page="currentPage"
-            :per-page="perPage" 
-            :filter="filter"
-            :filterIncludedFields="filterOn"
+            :items="recomendedTable.questions"
+            :fields="recomendedTable.fields"
+            :sort-by.sync="recomendedTable.sortBy"
+            :sort-desc.sync="recomendedTable.sortDesc"
+            :current-page="recomendedTable.currentPage"
+            :per-page="recomendedTable.perPage" 
+            :filter="recomendedTable.filter"
+            :filterIncludedFields="recomendedTable.filterOn"
+            head-variant="light"
+            @row-clicked="viewQuestion"
+            </b-table>
+            <br>
+            <div class="notice-layout">
+              <b-pagination style="float: left;" size="md" :per-page="perPage" :total-rows="totalRows" v-model="currentPage">
+              </b-pagination>
+            </div>
+          </b-col>
+        </b-row>
+      </div>
+    </div>
+
+    <div style="margin:30px;">
+      <b-input-group size="lg">
+        <div style="float:left;margin-right:30px;font-weight:bold;font-size:20px;line-height:40px;">
+          Search
+        </div>
+        <b-dropdown variant="info" size="lg" :text="questionTable.filterText" style="width:100px;">
+          <b-dropdown-item @click="questionTable.filterOn = ['title']; questionTable.filterText='Title'">Title</b-dropdown-item>
+          <b-dropdown-item @click="questionTable.filterOn = ['uid']; questionTable.filterText='Author'">Author</b-dropdown-item>
+        </b-dropdown>
+        <b-form-input
+          v-model="questionTable.filter"
+          type="search"
+          id="filterInput"
+          placeholder="Search..."
+        ></b-form-input>
+        <b-input-group-append>
+          <b-button :disabled="!questionTable.filter" @click="questionTable.filter = ''">Clear</b-button>
+        </b-input-group-append>
+      </b-input-group>
+    </div>
+
+    <div class="card">
+      <div class="card-body">
+        <b-row>
+          <b-col lg="12">
+            <b-table class="mb-0 table-outline" style="text-align: center" responsive="lg" hover 
+            :items="questionTable.questions"
+            :fields="questionTable.fields"
+            :sort-by.sync="questionTable.sortBy"
+            :sort-desc.sync="questionTable.sortDesc"
+            :current-page="questionTable.currentPage"
+            :per-page="questionTable.perPage" 
+            :filter="questionTable.filter"
+            :filterIncludedFields="questionTable.filterOn"
             head-variant="light"
             @row-clicked="viewQuestion"
             @filtered="onFiltered">
@@ -49,29 +83,56 @@ export default {
   data () {
     return {
       auth: false,
-      questions: [],
-      sortBy: 'id',
-      sortDesc: true,
-      currentPage: 1,
-      perPage: 5,
-      totalRows: 0,
-      filter: null,
-      filterOn: ['title'],
-      fields: [ 
-        { key: 'id', label: '번호', sortable: true},
-        { key: 'title', label: '제목', sortable: true },
-        { key: 'uid', label: '등록자', sortable: true},
-        { key: 'createdAt', label: '등록 날짜', sortable: true},
-        { key: 'star', label: '추천', sortable: true}
-      ]
+      isMentor: false,
+      questionTable: {
+        questions: [],
+        fields: [ 
+          { key: 'id', label: '번호', sortable: true},
+          { key: 'title', label: '제목', sortable: true },
+          { key: 'uid', label: '등록자', sortable: true},
+          { key: 'createdAt', label: '등록 날짜', sortable: true},
+          { key: 'star', label: '추천', sortable: true}
+        ],
+        sortBy: 'id',
+        sortDesc: true,
+        currentPage: 1,
+        perPage: 5,
+        totalRows: 0,
+        filter: null,
+        filterOn: ['title'],
+        filterText: 'Title'
+      },
+      recomendedTable: {
+        questions: [],
+        fields: [ 
+          { key: 'id', label: '번호', sortable: true},
+          { key: 'title', label: '제목', sortable: true },
+          { key: 'uid', label: '등록자', sortable: true},
+          { key: 'createdAt', label: '등록 날짜', sortable: true},
+          { key: 'star', label: '추천', sortable: true}
+        ],
+        sortBy: 'id',
+        sortDesc: true,
+        currentPage: 1,
+        perPage: 5,
+        totalRows: 0,
+        filter: null,
+        filterOn: ['title'],
+      }
     };
 
   },
   mounted: async function(){
       this.checkSession();
       const qlist = await this.$http.get('/rest/question/list');
-      this.questions = qlist.data.questions;
-      this.totalRows = this.questions.length;
+      this.questionTable.questions = qlist.data.questions;
+      this.questionTable.totalRows = this.questionTable.questions.length;
+      if(isMentor)
+      {
+        const rlist = await this.$http.get('/rest/question/listByTime');
+        this.recomendedTable.questions = rlist.data.questions;
+        this.recomendedTable.totalRows = this.recomendedTable.questions.length;
+      }
   },
   methods: {
     viewQuestion: async function(item, index){
@@ -83,15 +144,19 @@ export default {
         if(session.data.result == false){
           this.$router.push({'path' : '/'});
         }
+        else {
+          this.isMentor = (session.data.user.type == "Mentor");
+        }
       } catch(e){
+        alert(e);
         this.$router.push({'path' : '/'});
       }
 
     },
     onFiltered(filteredItems) {
       // Trigger pagination to update the number of buttons/pages due to filtering
-      this.totalRows = filteredItems.length
-      this.currentPage = 1
+      this.questionTable.totalRows = filteredItems.length
+      this.questionTable.currentPage = 1
     }
   }
 }
