3c9730845f5c8c4179a9a5cf54af26a439db2fd6
D-Sinus <peoplebj@naver.com>
2019-12-08 05:03:45 +0900

    [Add] Algorithm for reverse-parse available time array

--- source/src/views/answer/ArrangeTime.vue
+++ source/src/views/answer/ArrangeTime.vue
@@ -1,9 +1,13 @@
 <template>
-    <div class="arrangeTime" style="margin:100px 40px 40px 40px">
-        <h2><p>ArrangeTime</p></h2>
-        <div class="timeSelect">
-            <p text-align: center>Selected Time : {{ selectedDay }} / {{ selectedTime }} </p>
-            <div>
+    <div class="arrangeTime" style="margin-top:100px">
+        <h2><p style="margin-left:50px; margin-bottom:10px">ArrangeTime</p></h2>
+        <div class="menteeSchedule">
+        </div>
+        <div class="formTime">
+            <h5><p text-align: center>You can arrange time, based on mentee's schedule </p></h5>
+            <h5><p text-align: center>Choose time schedule you want to answer </p></h5>
+            <h5><p text-align: center>Selected Time : {{ selectedDay }} / {{ selectedTime }} </p></h5>
+            <div class="daySelect">
                 <b-form-group style="margin-bottom: 10px">
                     <b-form-radio-group v-model="selectedDay" :options="days"
                     size="lg" buttons button-variant="outline-success">
@@ -11,22 +15,26 @@
                 </b-form-group>
             </div>
             <div style="clear: both; margin-top: 10px"></div>
-            <timeselector class="form-control" displayFormat="  HH : mm  " :id="selectedTime" :interval="{h:1, m:5}"
-            :displaySeconds="false" style="width:408px; float: left" :placeholder="'[From] HH:MM'"
-            returnFormat="HH:mm" @formatedTime="tSelected">
-                <template slot="hours">
-                    <span>Hours</span>
-                </template>
-                <template slot="minutes">
-                    <span>Minutes</span>
-                </template>
-            </timeselector>
-            <img src="../../assets/datatables/images/add_circle.png" v-on:click="setTime(timeFormat)" style="cursor:pointer">
-            <div style="clear: both; margin-top: 10px"></div>
+            <div class="formTimePicker">
+                <timeselector class="timeSelect form-control" displayFormat="  HH : mm  " :id="selectedTime" :interval="{h:1, m:5}"
+                :displaySeconds="false" style="width:408px" :placeholder="'Choose your schedule : HH:MM'"
+                returnFormat="HH:mm" @formatedTime="tSelected">
+                    <template slot="hours">
+                        <span>Hours</span>
+                    </template>
+                    <template slot="minutes">
+                        <span>Minutes</span>
+                    </template>
+                </timeselector>
+                <img src="../../assets/datatables/images/add_circle.png" v-on:click="setTime(timeFormat)" style="cursor:pointer">
+            </div>
+        </div>
+        <div id="buttonHolder" style="margin:10px">
+            <b-button type="submit" variant="success" size="sm" @click.prevent="getTime()">Get Schedule</b-button>
         </div>
         <div style="clear: both"></div>
         <div id="buttonHolder" style="margin:10px">
-            <b-button type="submit" variant="success" size="sm" @click.prevent="submitTime()">시간 제출하기</b-button>
+            <b-button type="submit" variant="success" size="sm" @click.prevent="submitTime()">Submit Schedule</b-button>
         </div>
     </div>
 </template>
@@ -43,6 +51,8 @@ export default {
         return {
             qid: 0,
             available_times: [],
+            parsedAD: [],
+            parsedAT: [],
             timeFormat: '',
             selectedDay: '',
             selectedTime: '',
@@ -58,13 +68,12 @@ export default {
             ],
         }
     },
-    // created: async function () {
-    //     const questionInfo = await this.$http.get('rest/question/'+this.$route.params.id);
-        
-    // },
-    // mounted: async function () {
-
-    // },
+    created: async function () {
+        const scheduleInfo = await this.$http.get('/rest/question/availableTime/'+this.$route.params.qid);
+        console.log(scheduleInfo);
+        console.log(scheduleInfo.data.available_times);
+        this.available_times = scheduleInfo.data.available_times;
+    },
     methods: {
         async submitTime () {
             try{
@@ -82,6 +91,12 @@ export default {
         },
         tSelected (e) {
             this.selectedTime = e;
+        },
+        getTime () {
+            this.parsedAD = this.dayParse(this.available_times);
+            this.parsedAT = this.timeParse(this.parsedAD);
+            console.log(this.parsedAD);
+            console.log(this.parsedAT);
         },
         setTime (timeFormat) {
             if (this.timeCheck() == false) {
@@ -140,16 +155,82 @@ export default {
             }
             else return -1;
         },
-        timeToNum(time){
+        timeToNum(time) {
             var hmArr = time.split(":");
             hmArr[0] = Number(hmArr[0]);
             hmArr[1] = Number(hmArr[1]);
             return ((hmArr[0] * 12) + (hmArr[1] / 5));
+        },
+        numToTime(num) {
+            var hour = parseInt(((num % 288) - 1) / 12);
+            var min = parseInt(((num % 288) - 1) / 24);
+            return (hour + ":" + min);
+        },
+        dayParse (numArr) {
+            const arr = numArr;
+            var day = new Array();
+            for (var i = 0; i < 7; i++){
+                day[i] = new Array();
+            }
+            for (var i = 0; i < arr.length; i++){
+                switch (parseInt(arr[i] / 288)){
+                    case 0: 
+                        day[0].push(arr[i]);
+                        break;
+                    case 1:
+                        day[1].push(arr[i]);
+                        break;
+                    case 2:
+                        day[2].push(arr[i]);
+                        break;
+                    case 3:
+                        day[3].push(arr[i]);
+                        break;
+                    case 4:
+                        day[4].push(arr[i]);
+                        break;
+                    case 5:
+                        day[5].push(arr[i]);
+                        break;
+                    default:
+                        day[6].push(arr[i]);
+                        break;
+                }        
+            }
+            return day;
+        },
+        timeParse (numArr) {
+            const arr = numArr;
+            var day = new Array();
+            var timeStart = 0;
+            var next = 0;
+            var count = 0;
+            for (var i = 0; i < 7; i++){
+                day[i] = new Array();
+            }
+            for (var i = 0; i < 7; i++){
+                for (var j = 0; j < arr[i].length; j++){
+                    if (count == 0){
+                        timeStart = arr[i][j];
+                        next = timeStart + 1;
+                        count++;
+                    }
+                    else if ((next != arr[i][j]) || (j == arr[i].length - 1)) {
+                        day[i].push(this.numToTime(timeStart) + " : " + this.numToTime(timeStart + count));
+                        count = 0;
+                    }
+                    else {
+                        next++;
+                        count++;
+                    }
+                }
+            }
+            return day;
         }
     }
 }
 </script>
 
 <style lang="scss" scoped>
-@import "../../assets/css/common/registerquestion.css";
+@import "../../assets/css/common/arrangetime.css";
 </style>
