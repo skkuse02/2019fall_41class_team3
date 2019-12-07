13a986f045c08095db6e40ef2400804fafba87d0
devAdaid <ahyeong2533@gmail.com>
2019-12-07 19:22:54 +0900

    [Edit] Sign up with time select

--- source/src/views/user/Signup.vue
+++ source/src/views/user/Signup.vue
@@ -53,6 +53,45 @@
             <div class="content"><input type="text" class="form-control" v-model="email"/></div>
             <div class="message">{{ validation.firstError('email') }}</div>
           </div>
+          
+          <div class="form-vif" v-if="type=='Mentor'">
+              <p text-align: center>Selected Time Range : {{ selectedDay }} / {{ timeStart }} ~ {{ timeEnd }}</p>
+              <div>
+                  <b-button-group size="lg" style="margin-bottom: 10px">
+                      <b-button v-for="(day, idx) in days" :key="idx"
+                      :pressed.sync="day.state" variant="success">
+                          {{ day.text }}
+                      </b-button>
+                  </b-button-group>
+              </div>
+              <div style="clear: both; margin-top: 10px"></div>
+              <timeselector class="form-control" displayFormat=" [From] HH : mm" :id="timeStart" :interval="{h:1, m:5}"
+              :displaySeconds="false" style="width:208px; float: left" :placeholder="'[From] HH:MM'"
+              returnFormat="HH:mm" @formatedTime="tStart">
+                  <template slot="hours">
+                      <span>Hours</span>
+                  </template>
+                  <template slot="minutes">
+                      <span>Minutes</span>
+                  </template>
+              </timeselector>
+              <timeselector class="form-control" displayFormat=" [To] HH : mm" :id="timeEnd" :interval="{h:1, m:5}"
+              :displaySeconds="false" style="width:208px; float: left" :placeholder="'[To] HH:MM'"
+              returnFormat="HH:mm" @formatedTime="tEnd">
+                  <template slot="hours">
+                      <span>Hours</span>
+                  </template>
+                  <template slot="minutes">
+                      <span>Minutes</span>
+                  </template>
+              </timeselector>
+              <img src="../../assets/datatables/images/add_circle.png" v-on:click="addTime(timeFormat)" style="cursor:pointer">
+              <div style="clear: both; margin-top: 10px"></div>
+              <div class="timeChip">
+                  <vs-chip v-on:click="removeTime(time)" v-for="time in available_times"
+                  v-bind:key="time" style="width:40%; float:left" closable> {{ time }} </vs-chip>
+              </div>
+          </div>
       </div>
 
       <!--  -->
@@ -68,7 +107,7 @@
         </div>
       </div>
 
-      <div>
+      <div style="height:30px;margin:50px;">
         <div style="float:left;">
             <button class="btn btn-primary" v-if="isPrevBtnShow" type="button" @click="setStepDiff(-1)">&lt; Prev</button>
         </div>
@@ -88,8 +127,13 @@
 </template>
 
 <script>
+import Timeselector from 'vue-timeselector';
+
 export default {
   name: "Signup",
+  components: {
+      Timeselector
+  },
   data () {
     return {
         currentStep: 0,
@@ -107,7 +151,40 @@ export default {
         fields: [],
         available_times: [],
         repeat: '',
+        timeFormat: '',
+        timeStart: '',
+        timeEnd: '',
+        selectedDays: [],
+        days: [
+            { text: 'Mon', state: false, value: 'Monday' }, // val = 0
+            { text: 'Tue', state: false, value: 'Tuesday' }, // val = 288
+            { text: 'Wed', state: false, value: 'Wednesday' }, // val = 576
+            { text: 'Thu', state: false, value: 'Thursday' }, // val = 864
+            { text: 'Fri', state: false, value: 'Friday' }, // val = 1152
+            { text: 'Sat', state: false, value: 'Saturday' }, // val = 1440
+            { text: 'Sun', state: false, value: 'Sunday' } // val = 1728                
+        ]
     };
+  },
+  computed: {
+      answerType: function () {
+          if (this.typeIndex == 0) {
+              return false;
+          }
+          else if (this.typeIndex == 1) {
+              return false;
+          }
+          else return true;
+      },
+      selectedDay: function () {
+          this.selectedDays = [];
+          for (var i = 0; i < 7; i++){
+              if (this.days[i].state == true){
+                  this.selectedDays.push(this.days[i].value);
+              }
+          }
+          return this.selectedDays;
+      },
   },
   validators: {
     uid: function (value) {
@@ -202,7 +279,7 @@ export default {
           const nickname = this.nickname;
           const email = this.email;
           const fields = this.fields;
-          const available_times = this.available_times;
+          const available_times = this.timeFormatTransform();
           const reqRes = await this.$http.post("/rest/user", {uid, type, password, name, nickname, email, fields, available_times})
           if (reqRes.data.result == true) {
             this.$modal.show('welcome-user');
@@ -222,7 +299,98 @@ export default {
       this.$router.push({
         path: '/login'
       });
+    },
+    
+    tStart (e) {
+        this.timeStart = e;
+    },
+    tEnd (e) {
+        this.timeEnd = e;
+    },
+    addTime (timeFormat) {
+        if (this.timeCheck() == false) {
+            alert('요일과 시간을 다시 한 번 확인해 주세요!');
+        }
+        else {
+            this.timeFormat = this.selectedDays + '/' + this.timeStart + '~'  + this.timeEnd; 
+            console.log(this.timeFormat);
+            this.available_times.push(this.timeFormat)
+            this.timeFormat = null;
+        }
+    },
+    removeTime (time) {
+        this.available_times.splice(this.available_times.indexOf(time), 1)
+    },
+    timeCheck () {
+        if ((this.selectedDays.length == 0) || (this.timeStart == '') || (this.timeEnd == '')){
+            return false;
+        }
+        else if (this.timeStart >= this.timeEnd) {
+            return false;
+        }
+        else return true;
+    },
+    timeFormatTransform () {
+        const timeArr = this.available_times;
+        var spArr = [];
+        var daArr = [];
+        var tiArr = [];
+        var stime = 0;
+        var etime = 0;
+        var resultArr = [];
+        for (var i = 0; i < timeArr.length; i++) {
+            spArr = timeArr[i].split("/");  // s[0] = "days", s[1] = "times"
+            daArr = spArr[0].split(",");    // d[0] = "day1", d[2] = "day2", ...
+            tiArr = spArr[1].split("~");    // t[0] = "h:m(s)", t[1] = "h:m(e)"
+            stime = this.timeToNum(tiArr[0]);
+            etime = this.timeToNum(tiArr[1]);
+            for (var j = 0; j < daArr.length; j++){
+                daArr[j] = this.dayToNum(daArr[j]);  // d[0] = Num(day1), ...
+                for (var k = stime; k < etime; k++){
+                    resultArr.push(daArr[j] + k);
+                }
+            }   // Transform daArr's content into number type
+            console.log(spArr);
+            console.log(daArr);
+            console.log(tiArr);
+            console.log(stime, etime);
+        }
+        return resultArr.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+    },
+    dayToNum (day) {
+        if (day == "Monday") {
+            return 0;
+        }
+        else if (day == "Tuesday") {
+            return 288;
+        }
+        else if (day == "Wednesday") {
+            return 576;
+        }
+        else if (day == "Thursday") {
+            return 864;
+        }
+        else if (day == "Friday") {
+            return 1152;
+        }
+        else if (day == "Saturday") {
+            return 1440;
+        }
+        else if (day == "Sunday") {
+            return 1728;
+        }
+        else return -1;
+    },
+    timeToNum(time){
+        var hmArr = time.split(":");
+        hmArr[0] = Number(hmArr[0]);
+        hmArr[1] = Number(hmArr[1]);
+        return ((hmArr[0] * 12) + (hmArr[1] / 5));
     }
   }
 };
 </script>
+
+<style lang="scss" scoped>
+@import "../../assets/css/common/registerquestion.css";
+</style>
